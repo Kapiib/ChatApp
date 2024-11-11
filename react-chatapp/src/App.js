a46c6ddb4e87cc0f51500f4c5290b7acdada0cea
Pimser <142997610+Pimser@users.noreply.github.com>
2024-11-11 09:00:25 +0100

    mandag 09:00 push

--- react-chatapp/src/App.js
+++ react-chatapp/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import About from "./pages/about";
-import About from "./pages/about";
+import About from "./pages/About";
+
 
 function App() {
   return (
