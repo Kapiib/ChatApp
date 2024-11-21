8a17d3f843ef24bb6f6a7b5d8759b02c14fcc930
Pimser <142997610+Pimser@users.noreply.github.com>
2024-11-21 12:05:23 +0100

    torsdag endring uke 46 2024,, Lagde interface og create user

--- react-chatapp/src/pages/LandingPage.js
+++ react-chatapp/src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 export default function LaningPage() {
     return(
         <div>
-            <h1>Welcome to YabHub!</h1>
+            <h1>Welcome to YapHub!</h1>
             <h2>Yap Your Way To Connection</h2>
         </div>
     )
